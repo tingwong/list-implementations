069c790476fa7dfefc9e30b106efc1c8e41efe75
Crystal Hess <crystal@adadevelopersacademy.org>
2017-04-18 11:40:45 -0700

    solution to array-list

--- array-list.rb
+++ array-list.rb
@@ -2,29 +2,51 @@
 
 class ArrayList
   def initialize
-    @storage = []
+    @storage = [0,0,0,0,0,0,0,0,0,0]
+    @size = 0
   end
 
   # Adds _value_ at the end of the list
   def add(value)
+    @storage[@size] = value
+    @size += 1
   end
 
   # Deletes the _last_ value in the array
   def delete
+#    @storage[@size-1] = 0
+    @size -= 1
   end
 
   def display
+    toReturn = "["
+    @size.times do |i|
+      toReturn += @storage[i].to_s + ", "
+    end
+    puts toReturn + "]"
   end
 
   def include?(key)
+    @size.times do |i|
+      return true if @storage[i] == key
+    end
+    return false
   end
 
   def size
+    return @size
   end
 
   def max
+    return nil if @size == 0
+    max = @storage[0]
+    @size.times do |i|
+      if @storage[i] > max
+        max = @storage[i]
+      end
+    end
+    return max
   end
-
 end
 
 # Initializing an Array List
